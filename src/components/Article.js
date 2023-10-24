f4c14f28fb5d3feef522083b75712f270b82f110
Markswell <osoromarkswell80@gmail.com>
2023-10-24 21:55:38 +0300

    Bonus Deliverable

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,8 +5,9 @@ function Article(props) {
   return (
     <article>
       <h3>{props.title}</h3>
-      <small>{props.date || "January 1, 1970"}</small>
+      <small>{props.date || "January 1, 1970"} {props.emojis.join(' ')} {props.minutes} min read</small>
       <p>{props.preview}</p>
+      
     </article>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,15 +4,29 @@ import Article from "./Article";
 function ArticleList(props) {
   return (
     <main>
-      {props.posts.map((post) => (
-        <Article
-          key={post.id} 
-          title={post.title}
-          date={post.date || "January 1, 1970"}
-          preview={post.preview}
-        />
-      ))}
+      {props.posts.map((post) => {
+        // Calculate the number of coffee cups and bento boxes based on the read time
+        const minutesToRead = post.minutes;
+        const coffeeCups = Math.ceil(minutesToRead / 5);
+        const bentoBoxes = Math.ceil(minutesToRead / 10);
+
+        // Create an array of coffee cup and bento box emojis
+        const coffeeCupEmojis = Array(coffeeCups).fill("☕️");
+        const bentoBoxEmojis = Array(bentoBoxes).fill("🍱");
+
+        return (
+          <Article
+            key={post.id} 
+            title={post.title}
+            date={post.date || "January 1, 1970"}
+            preview={post.preview}
+            minutes={post.minutes}
+            emojis={[...coffeeCupEmojis, ...bentoBoxEmojis]}
+          />
+        );
+      })}
     </main>
   );
 }
+
 export default ArticleList;
